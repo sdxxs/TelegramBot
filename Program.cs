502a80cc27517b04d61346e135c128e2b7d986b9
sdxxs <sarabunka@gmail.com>
2024-06-01 11:47:27 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using TelegramBot.TGClient;
+
+namespace TelegramBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var botClient = new TelegramBotClient("6981602280:AAHke33WLZeHd7wjzcDR3WdglqhfgFhV_2g");
+            botClient.StartReceiving(Update, Error);
+            Console.ReadLine();
+
+        }
+
+        async static Task Update(ITelegramBotClient botClient, Telegram.Bot.Types.Update update, CancellationToken token)
+        {
+
+            if (update.Type != UpdateType.Message)
+            {
+                return;
+            }
+            if (update.Message!.Type != MessageType.Text)
+            {
+                return;
+            }
+            var message = update.Message;
+            if (message.Text != null)
+            {
+                await Handlermessage(botClient, message, token);
+            }
+        }
+        async static Task Handlermessage(ITelegramBotClient botClient, Message message, CancellationToken token)
+        {
+            TelegramClient telegramBotClient = new TelegramClient(botClient);
+
+            if (message.Text.Contains("/start"))
+            {
+                await telegramBotClient.GreetingMessage(message);
+
+            }
+
+            else if (message.Text.Contains("/observ"))
+            {
+                await telegramBotClient.GetОbservForRegion(message);
+
+            }
+
+            else if (message.Text.Contains("/getlistofregioncodeukraine"))
+            {
+                await telegramBotClient.GetListOfRegionCodeUkraine(message);
+            }
+
+            else if (message.Text.Contains("/addmyobserv"))
+            {
+                await telegramBotClient.AddMyObserv(message);
+            }
+
+            else if (message.Text.Contains("/checkmyobservlist"))
+            {
+                await telegramBotClient.CheckMyObservathionList(message);
+            }
+
+            else if (message.Text.Contains("/removeobservfrommylist"))
+            {
+                await telegramBotClient.RemoveObservFromMyList(message);
+            }
+
+            else if (message.Text.Contains("/photo"))
+            {
+                await telegramBotClient.Photo(message);
+            }
+
+        }
+        private static Task Error(ITelegramBotClient botClient, Exception exception, CancellationToken token)
+        {
+            var ErroeMessage = exception switch
+            {
+                ApiRequestException apiRequestException => $"Помилка в телеграм бот АПІ:\n{apiRequestException.ErrorCode}" +
+                $"\n {apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+            Console.WriteLine(ErroeMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ TGClient/TelegramClient.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TelegramBot.TGClient
+{
+    internal class TelegramClient
+    {
+        private ITelegramBotClient botClient { get; set; }
+        public TelegramClient(ITelegramBotClient _botClient)
+        {
+            botClient = _botClient;
+        }
+
+       
+        public async Task GreetingMessage(Message message)
+        {
+            await botClient.SendTextMessageAsync(
+            chatId: message.Chat.Id,
+            text: "Вітаємо вас в боті для спостереженням за птахами\\! \n\nЗа допомогою *команд* ти зможеш як отримувати всю потрібну інформацію про спостереження\\, так і створити свій власний список\\.",
+            parseMode: ParseMode.MarkdownV2,
+            disableNotification: true,
+            replyToMessageId: message.MessageId,
+            replyMarkup: new InlineKeyboardMarkup(
+            InlineKeyboardButton.WithUrl(
+            text: "Більше цікавої інформації на сайті",
+            url: "https://ebird.org/home")));
+
+        }
+
+        public async Task GetОbservForRegion(Message message)
+        {
+
+            string RegionCode = message.Text.Substring(message.Text.IndexOf("v") + 1);
+
+            string path = $"http://localhost:5122/api/MyBird/ObservByRegionCode?regionCode={RegionCode}";
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(path),//address            
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                string ListOfObserv = await response.Content.ReadAsStringAsync();
+
+                await botClient.SendTextMessageAsync(chatId: message.Chat.Id,
+                text: ListOfObserv,
+                disableNotification: true,
+                replyToMessageId: message.MessageId);
+            }
+        }
+
+
+        public async Task GetListOfRegionCodeUkraine(Message message)
+        {
+
+            string path = $"http://localhost:5122/api/MyBird/GetRegionList";
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(path),//address            
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                string ListOfRegionCode = await response.Content.ReadAsStringAsync();
+                await botClient.SendTextMessageAsync(chatId: message.Chat.Id,
+                text: ListOfRegionCode,
+                disableNotification: true,
+                replyToMessageId: message.MessageId);
+            }
+
+        }
+
+        public async Task CheckMyObservathionList(Message message)
+        {
+            long ChatId = message.Chat.Id;
+            string path = $"http://localhost:5122/api/MyBird/GetMyOwnObservList?PersonChatId={ChatId}";
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(path),//address            
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                string ListOfMyObserv = await response.Content.ReadAsStringAsync();
+                await botClient.SendTextMessageAsync(chatId: message.Chat.Id,
+                text: ListOfMyObserv,
+                disableNotification: true,
+                replyToMessageId: message.MessageId);
+            }          
+        }
+
+
+        public async Task RemoveObservFromMyList(Message message)
+        {
+
+            string id = message.Text.Substring(message.Text.IndexOf("t") + 1);
+
+            string path = $"http://localhost:5122/api/MyBird/DeleteMyObserv?PersonChatId={message.Chat.Id}&id={id}";
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Delete,
+                RequestUri = new Uri(path),//address            
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                await botClient.SendTextMessageAsync(chatId: message.Chat.Id,
+                text: result,
+                parseMode: ParseMode.MarkdownV2,
+                disableNotification: true,
+                replyToMessageId: message.MessageId
+                );
+            }
+
+        }
+
+        public async Task AddMyObserv(Message message)
+        {
+            string[] info = message.Text.Split(";");
+            string result = " ";
+
+            if (info.Length != 6)
+            {
+                result = "Щось пішло не так\\.\\.\\.Ви допустили помилку в записі команди\\, ocь приклад щодо використання \\p\\.s обов\\'язково після кожної властивості *пишіть \\;* \\:" +
+             "\n /addmyobserv; _\n Назва птаха\\; \n Назва місця спостереження\\; \n Дата і час\\;  \n Кількість особин цього видy\\, які спостерігались\\;_";
+            }
+            else
+            {
+                long ChatId = message.Chat.Id;
+                string path = $"http://localhost:5122/api/MyBird/PostMyObserv?PersonChatId={ChatId}&sciName={info[1]}&locName={info[2]}&obsDt={info[3]}&howMany={info[4]}";
+                var client = new HttpClient();
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Post,
+                    RequestUri = new Uri(path),            
+                };
+
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    result = await response.Content.ReadAsStringAsync();            
+                }               
+            }
+            await botClient.SendTextMessageAsync(
+                      chatId: message.Chat.Id,
+                      text: result,
+                      parseMode: ParseMode.MarkdownV2,
+                      disableNotification: true,
+                      replyToMessageId: message.MessageId);
+        }
+        public async Task Photo(Message message)
+        {
+            string path = $"http://localhost:5122/api/MyBird/RandomPhotoOfBird";
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(path),//address            
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                string url = await response.Content.ReadAsStringAsync();
+
+                await botClient.SendPhotoAsync(
+                chatId: message.Chat.Id,
+                photo: InputFile.FromString(url),
+                caption: "`Тримайте фотографію`",
+                parseMode: ParseMode.MarkdownV2,
+                disableNotification: true,
+                replyToMessageId: message.MessageId,
+                replyMarkup: new InlineKeyboardMarkup(
+                InlineKeyboardButton.WithUrl(
+                text: "Більше на сайті",
+                url: "https://unsplash.com/s/photos/birds"))
+                 );
+            }
+
+        }
+
+    }
+}
